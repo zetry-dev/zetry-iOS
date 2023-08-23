b3685708eb5c922f77e9a4a853529a1559019743
Allie Kim <allie0147@gmail.com>
2023-08-23 16:34:06 +0900

    💄 Add resize to ZentryIcon

--- Projects/UserInterface/DesignSystem/Sources/Icons/ZentryIcon.swift
+++ Projects/UserInterface/DesignSystem/Sources/Icons/ZentryIcon.swift
@@ -9,16 +9,54 @@
 import SwiftUI
 
 public struct ZentryIcon: View {
+    public enum Size {
+        case larger
+        case smaller
+        case custom(CGSize)
+    }
+
     private let icon: DesignSystemImages
     private let foregroundColor: Color.ZentryColorSystem
+    private let size: ZentryIcon.Size
 
-    public init(_ icon: DesignSystemImages, foregroundColor: Color.ZentryColorSystem = .grayScale(.gray12)) {
+    public init(
+        _ icon: DesignSystemImages,
+        foregroundColor: Color.ZentryColorSystem = .grayScale(.gray12),
+        size: ZentryIcon.Size = .smaller
+    ) {
         self.icon = icon
         self.foregroundColor = foregroundColor
+        self.size = size
     }
 
     public var body: some View {
         icon.swiftUIImage
+            .resizable()
+            .frame(width: size.width, height: size.height)
             .foregroundColor(.zentry(foregroundColor))
+    }
+}
+
+extension ZentryIcon.Size {
+    var width: CGFloat {
+        switch self {
+        case .larger:
+            return 24
+        case .smaller:
+            return 20
+        case .custom(let size):
+            return size.width
+        }
+    }
+
+    var height: CGFloat {
+        switch self {
+        case .larger:
+            return 24
+        case .smaller:
+            return 20
+        case .custom(let size):
+            return size.height
+        }
     }
 }
