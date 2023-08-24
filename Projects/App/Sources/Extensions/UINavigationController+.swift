62d964325c30c54f0887dcbdb40020b0fe4ae88c
Allie Kim <allie0147@gmail.com>
2023-08-24 14:31:11 +0900

    ✨ Add UINavigationController extension

---
+++ Projects/App/Sources/Extensions/UINavigationController+.swift
@@ -0,0 +1,34 @@
+//
+//  UINavigationController+.swift
+//  Zentry
+//
+//  Created by AllieKim on 2023/08/24.
+//  Copyright © 2023 com.zentry. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}
+
+public func popToRoot() {
+    let window = UIApplication.shared.connectedScenes
+        .filter { $0.activationState == .foregroundActive }
+        .map { $0 as? UIWindowScene }
+        .compactMap { $0 }
+        .first?.windows
+        .filter(\.isKeyWindow)
+        .first
+
+    let profile = window?.rootViewController?.children.first as? UINavigationController
+
+    profile?.popToRootViewController(animated: true)
+}
