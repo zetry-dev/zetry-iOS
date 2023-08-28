2500fc06c157cd5726a83ee2c30a0459859007e2
Allie Kim <allie0147@gmail.com>
2023-08-28 19:11:19 +0900

    ✅ Add SearchFeature onAppear test

--- Projects/Feature/SearchFeature/Project.swift
+++ Projects/Feature/SearchFeature/Project.swift
@@ -5,14 +5,25 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.SearchFeature.rawValue,
     targets: [
-        .implements(module: .feature(.SearchFeature), dependencies: [
-            .feature(target: .BaseFeature)
-        ]),
-        .tests(module: .feature(.SearchFeature), dependencies: [
-            .feature(target: .SearchFeature)
-        ]),
-        .demo(module: .feature(.SearchFeature), dependencies: [
-            .feature(target: .SearchFeature)
-        ])
+        .implements(
+            module: .feature(.SearchFeature),
+            dependencies: [
+                .feature(target: .BaseFeature),
+                .domain(target: .CategoryDomain),
+                .domain(target: .CategoryDomain, type: .interface),
+            ]
+        ),
+        .tests(
+            module: .feature(.SearchFeature),
+            dependencies: [
+                .feature(target: .SearchFeature)
+            ]
+        ),
+        .demo(
+            module: .feature(.SearchFeature),
+            dependencies: [
+                .feature(target: .SearchFeature)
+            ]
+        ),
     ]
 )
