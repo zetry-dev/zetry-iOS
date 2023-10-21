799fa2d584a0484486c7e9d5d25f62bfdd0dfaa7
AllieKim <allie0147@gmail.com>
2023-10-21 23:31:38 +0900

    ✨ Add CategoryDomain and ProductDomain to CateogoryFeature

--- Projects/Feature/CategoryFeature/Project.swift
+++ Projects/Feature/CategoryFeature/Project.swift
@@ -5,15 +5,30 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Feature.CategoryFeature.rawValue,
     targets: [
-        .implements(module: .feature(.CategoryFeature), dependencies: [
-            .feature(target: .BaseFeature),
-            .feature(target: .SearchFeature),
-        ]),
-        .tests(module: .feature(.CategoryFeature), dependencies: [
-            .feature(target: .CategoryFeature),
-        ]),
-        .demo(module: .feature(.CategoryFeature), dependencies: [
-            .feature(target: .CategoryFeature),
-        ]),
+        .implements(
+            module: .feature(.CategoryFeature),
+            dependencies: [
+                .feature(target: .BaseFeature),
+                .feature(target: .SearchFeature),
+                .domain(target: .CategoryDomain),
+                .domain(target: .ProductDomain)
+            ]
+        ),
+        .tests(
+            module: .feature(.CategoryFeature),
+            dependencies: [
+                .feature(target: .CategoryFeature),
+                .domain(target: .CategoryDomain),
+                .domain(target: .ProductDomain)
+            ]
+        ),
+        .demo(
+            module: .feature(.CategoryFeature),
+            dependencies: [
+                .feature(target: .CategoryFeature),
+                .domain(target: .CategoryDomain),
+                .domain(target: .ProductDomain)
+            ]
+        ),
     ]
 )
