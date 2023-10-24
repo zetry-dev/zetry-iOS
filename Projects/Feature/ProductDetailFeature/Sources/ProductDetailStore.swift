15df481e1855dc0066bc364c250b7c12c15dff50
Allie Kim <allie0147@gmail.com>
2023-10-24 17:57:51 +0900

    ✨ Add ProductDetail feature

--- Projects/Feature/ProductDetailFeature/Sources/ProductDetailStore.swift
+++ Projects/Feature/ProductDetailFeature/Sources/ProductDetailStore.swift
@@ -7,14 +7,18 @@
 //
 
 import ComposableArchitecture
+import Foundation
 import ProductDomain
 import ProductDomainInterface
+import UIKit
 
 public struct ProductDetailStore: Reducer {
     public init() {}
 
     public struct State: Equatable {
         let item: ProductEntity
+        let imageHeight: CGFloat = UIScreen.main.bounds.height / 2.2
+        var scrollViewOffsetY: CGFloat = 0.0
 
         public init(item: ProductEntity) {
             self.item = item
@@ -22,17 +26,25 @@ public struct ProductDetailStore: Reducer {
     }
 
     public enum Action: Equatable {
-        case onAppear
+        case onLoad
+        case scrollOffsetYChanged(CGFloat)
+
+        case pop
+        case popToRoot
     }
 
     @Dependency(\.productClient) var productClient
 
     public var body: some ReducerOf<Self> {
-        Reduce { _, action in
+        Reduce { state, action in
             switch action {
-            case .onAppear:
+            case .onLoad:
                 return .none
-            default: return .none
+            case .scrollOffsetYChanged(let offset):
+                state.scrollViewOffsetY = offset
+                return .none
+            default:
+                return .none
             }
         }
     }
--- Projects/Feature/ProductDetailFeature/Sources/ProductDetailView.swift
+++ Projects/Feature/ProductDetailFeature/Sources/ProductDetailView.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2023 com.zetry. All rights reserved.
 //
 
+import BaseFeature
 import ComposableArchitecture
+import DesignSystem
+import ProductDomainInterface
 import SwiftUI
 
 public struct ProductDetailView: View {
@@ -17,8 +20,154 @@ public struct ProductDetailView: View {
     }
 
     public var body: some View {
-        WithViewStore(self.store) { $0 } content: { _ in
-            Text("Detail View")
+        WithViewStore(self.store) { $0 } content: { viewStore in
+            let product = viewStore.item
+
+            VStack(alignment: .leading, spacing: 0) {
+                ObservableScrollView(
+                    scrollOffset: viewStore.binding(
+                        get: \.scrollViewOffsetY,
+                        send: ProductDetailStore.Action.scrollOffsetYChanged
+                    )
+                ) { _ in
+                    GeometryReader { proxy in
+                        ZStack(alignment: .top) {
+                            productImageView(
+                                url: product.imageURL,
+                                imageHeight: viewStore.imageHeight,
+                                proxy: proxy
+                            )
+
+                            detailChips(
+                                recyclable: product.recyclable,
+                                isTrash: product.isTrash,
+                                category: product.category,
+                                imageHeight: viewStore.imageHeight
+                            )
+                        }
+                    }
+                    .frame(height: viewStore.imageHeight)
+                    detailContentView(product: product)
+                        .offset(y: -40)
+                }
+            }
+            .background(.regularMaterial)
+            .edgesIgnoringSafeArea(.all)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text(product.title)
+                        .fontStyle(.subtitle2)
+                        .opacity(viewStore.scrollViewOffsetY > 300 ? 1 : 0)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        viewStore.send(.pop)
+                    } label: {
+                        ZetryIcon(DesignSystemAsset.Icons.chevronLeft)
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        viewStore.send(.popToRoot)
+                    } label: {
+                        ZetryIcon(DesignSystemAsset.Icons.house)
+                    }
+                }
+            }
         }
+    }
+
+    @ViewBuilder
+    func productImageView(
+        url: String,
+        imageHeight: CGFloat,
+        proxy: GeometryProxy
+    ) -> some View {
+        let offsetY = proxy.frame(in: .global).minY > 0 ? -proxy.frame(in: .global).minY : 0
+        let height = proxy.frame(in: .global).minY > 0 ?imageHeight + proxy.frame(in: .global).minY : imageHeight
+
+        Image
+            .load(url)
+            .offset(y: offsetY)
+            .frame(height: height)
+    }
+
+    @ViewBuilder
+    private func detailChips(
+        recyclable: Bool,
+        isTrash: Bool,
+        category: String,
+        imageHeight: CGFloat
+    ) -> some View {
+        HStack(spacing: 8) {
+            DetailChip(
+                text: recyclable ? "재활용 가능" : "재활용 불가능",
+                icon: recyclable ? DesignSystemAsset.Icons.arrow3Trianglepath : nil
+            )
+            if isTrash {
+                DetailChip(text: "일반 쓰레기")
+            }
+            DetailChip(text: category)
+            Spacer()
+        }
+        .padding(.horizontal, 28)
+        .offset(y: imageHeight - 75)
+    }
+
+    @ViewBuilder
+    func detailContentView(product: ProductEntity) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text(product.title)
+                .fontStyle(.headline1)
+                .padding(.leading, 28)
+
+            Divider(color: .primary(.black), opacity: 0.05)
+
+            Text("버리는 방법")
+                .fontStyle(.subtitle1)
+                .padding(.leading, 28)
+
+            VStack(alignment: .leading, spacing: 16) {
+                ForEach(product.description, id: \.self) { description in
+                    BulletText(description)
+                }
+                HStack(alignment: .top, spacing: 8) {
+                    ZetryIcon(DesignSystemAsset.Icons.exclamationmarkCircle,
+                              foregroundColor: .primary(.primary))
+                    Text("마우스는 폐가전 무상방문수거 서비스를 통해 컴퓨터를 배출할 때 배출할 수 있어요.")
+                        .fontStyle(.body2, foregroundColor: .grayScale(.gray7))
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 9)
+
+            Divider(color: .grayScale(.gray2))
+
+            VStack(alignment: .leading) {
+                Text("이런 쓰레기는 어떠세요?")
+                    .fontStyle(.subtitle2)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 18) {
+                        CategoryItemCell("신문지", imageUrl: "ii", size: 72) {}
+                        CategoryItemCell("신문지", imageUrl: "ii", size: 72) {}
+                        CategoryItemCell("신문지", imageUrl: "ii", size: 72) {}
+                        CategoryItemCell("신문지", imageUrl: "ii", size: 72) {}
+                    }
+                }
+            }
+            .padding(.leading, 16)
+            .padding(.top, 13)
+        }
+        .padding(.top, 20)
+        .background(.regularMaterial)
+        .border(width: 1, edges: [.top], color: .white.opacity(0.8))
+        .cornerRadius(20, corners: [.topLeft, .topRight])
     }
 }
